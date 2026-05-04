fa1b7ddb709f69305ae3ab464b6c7c4ec6a89841
cwchoi <cwchoi@osci.kr>
2026-05-04 14:56:30 +0900

    feature: GlobalExceptionHandler 구현 및 테스트 코드 작성

---
+++ member/src/main/java/cwchoiit/splearn/member/adapter/in/web/common/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package cwchoiit.splearn.member.adapter.in.web.common;
+
+import cwchoiit.splearn.member.domain.DuplicateEmailException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ProblemDetail emailExceptionHandler(DuplicateEmailException exception) {
+        return getProblemDetail(HttpStatus.CONFLICT, exception);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleException(Exception exception) {
+        return getProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    }
+
+    private static @NonNull ProblemDetail getProblemDetail(HttpStatus status, Exception exception) {
+        ProblemDetail problemDetail =
+                ProblemDetail.forStatusAndDetail(status, exception.getMessage());
+
+        problemDetail.setProperty("exception", exception.getClass().getSimpleName());
+        problemDetail.setProperty("timestamp", LocalDateTime.now());
+
+        return problemDetail;
+    }
+}
--- member/src/test/java/cwchoiit/splearn/member/adapter/in/web/MemberControllerTest.java
+++ member/src/test/java/cwchoiit/splearn/member/adapter/in/web/MemberControllerTest.java
@@ -1,33 +1,33 @@
 package cwchoiit.splearn.member.adapter.in.web;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import cwchoiit.splearn.member.application.provided.MemberRegisterUseCase;
-import cwchoiit.splearn.member.domain.Member;
+import cwchoiit.splearn.member.application.required.MemberRepository;
 import cwchoiit.splearn.member.domain.MemberFixture;
 import cwchoiit.splearn.member.domain.payload.MemberRegisterPayload;
+import cwchoiit.splearn.member.domain.vo.Email;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import tools.jackson.databind.ObjectMapper;
 
+@Transactional
 @SpringBootTest
 class MemberControllerTest {
 
-    @MockitoBean MemberRegisterUseCase memberRegisterUseCase;
-
+    @Autowired MemberRegisterUseCase memberRegisterUseCase;
+    @Autowired MemberRepository memberRepository;
     @Autowired WebApplicationContext wac;
-
     @Autowired ObjectMapper objectMapper;
 
     MockMvc mockMvc;
@@ -39,9 +39,6 @@ class MemberControllerTest {
 
     @Test
     void register() throws Exception {
-        Member member = MemberFixture.createMember(1L);
-        when(memberRegisterUseCase.register(any())).thenReturn(member);
-
         MemberRegisterPayload payload =
                 MemberFixture.createMemberRegisterPayload("noreply@example.com");
         String requestJson = objectMapper.writeValueAsString(payload);
@@ -51,9 +48,9 @@ class MemberControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(requestJson))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.memberId").value(1));
+                .andExpect(jsonPath("$.email").value("noreply@example.com"));
 
-        verify(memberRegisterUseCase, times(1)).register(any());
+        assertThat(memberRepository.findByEmail(new Email("noreply@example.com"))).isPresent();
     }
 
     @Test
@@ -67,5 +64,20 @@ class MemberControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(requestJson))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void duplicateEmail() throws Exception {
+        MemberRegisterPayload payload =
+                MemberFixture.createMemberRegisterPayload("noreply@example.com");
+        memberRegisterUseCase.register(payload);
+
+        // 이메일 중복 요청
+        String requestJson = objectMapper.writeValueAsString(payload);
+        mockMvc.perform(
+                        post("/api/v1/members")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestJson))
+                .andExpect(status().isConflict());
     }
 }
